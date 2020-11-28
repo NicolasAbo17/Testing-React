44f7bfd3f6fc1b9a078cdcababde8040b70bd49c
casalazara <ca.salazara@uniandes.edu.co>
2020-11-28 14:34:01 -0500

    Listo (?)

--- react-test-app/src/App.js
+++ react-test-app/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Like from "./components/like/Like";
-
+import Like from "./components/like";
+import Checkbox from "./components/checkbox";
 function App() {
   return (
     <>
+    <Checkbox labelActive="Active" labelInactive="Inactive" />
     <Like/>
     </>
   );
 }
-
 export default App;
